4f1a502c27dc38f6654a45f545d8b51ab970f6d3
vleuschen <10719217@qq.com>
2020-08-12 23:45:54 +0800

    1、SpringCloud第八讲：支付模块构建（中）

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping("/save")
+    public CommonResult save(@RequestBody Payment payment){
+
+        int result = paymentService.save(payment);
+        log.info("****插入结果{}",result);
+
+        if(result > 0){
+            return new CommonResult(200,"插入数据成功",result);
+        }else{
+            return new CommonResult(444,"插入数据失败",null);
+        }
+    }
+
+    @GetMapping("/getPaymentId/{id}")
+    public CommonResult getPaymentId(@PathVariable("id")Long id){
+
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("****查询结果{}",payment.toString());
+
+
+        if(payment != null){
+            return new CommonResult(200,"查询成功",payment);
+        }else {
+            return new CommonResult(444,"查询失败",null);
+        }
+
+    }
+
+}
