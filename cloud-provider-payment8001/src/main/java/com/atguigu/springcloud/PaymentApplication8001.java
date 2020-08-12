b82ffa9914671fe6c116f83a948c671bdea0d451
vleuschen <10719217@qq.com>
2020-08-12 23:08:54 +0800

    1、SpringCloud第八讲：支付模块构建（上）

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentApplication8001.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication8001.class,args);
+    }
+}
